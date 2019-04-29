ee5a27e09c2b54bd47d3a5294083947167cb663b
Rutorai <rutorai.pf@yandex.com>
2019-04-29 20:34:01 +0200

    Script to retrieve all js files from nas, downlaod them and beautify them locally

---
+++ getjs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+HOST_USER=${HOST_USER:-admin}
+HOST_NAME=${HOST_NAME:-192.168.1.46}
+HOST_PORT=${HOST_PORT:-522}
+
+ssh -p $HOST_PORT $HOST_USER@$HOST_NAME << __EOS__
+if [ -e ~/javascript_files.tar ]
+then
+    rm -f ~/javascript_files.tar
+fi
+
+find /usr/syno/synoman -name *.js -exec tar -rf ~/javascript_files.tar {} \; 2> /dev/null
+__EOS__
+
+mkdir -p ./js/beautified
+
+sftp $HOST_USER@$HOST_NAME:/homes/$HOST_USER/javascript_files.tar ./js/
+(cd ./js; tar xf javascript_files.tar)
+
+(
+cd ./js
+find ./ \( -path "./beautified" \) -prune -o -name *.js 2> /dev/null | while read -r i;
+do
+    if [ ! -d $i ];
+    then
+        target=${i#"./"}
+        basedir=$(dirname "$target")
+        mkdir -p "beautified/$basedir"
+        js-beautify $i > beautified/$target
+    fi
+done
+)
+
+echo "Finished"
